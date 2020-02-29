7db71cee849125496d65440bd09946a411640034
websupergirl <38568655+websupergirl@users.noreply.github.com>
2020-02-29 13:43:31 -0800

    Update Logo.js

--- client/src/components/Header/Logo.js
+++ client/src/components/Header/Logo.js
@@ -2,24 +2,22 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 import { headerItem } from '../shared/helpers';
-import logo from './iuvo.png';
-
 
 const Logo = styled(Link)`
   ${headerItem};
-
+  
   margin-right: auto;
   font-size: 24px;
   font-weight: 500;
   color: ${props => props.theme.normalText};
   text-decoration: none;
-
+  
   @media (max-width: 425px) {
     padding: 0 8px 0 16px;
     font-size: 19px;
   }
 `;
 
-const HeaderLogo = () => <Logo> img src={Logo} </Logo>;
+const HeaderLogo = () => <Logo to='/'>asperitas</Logo>;
 
 export default HeaderLogo;
