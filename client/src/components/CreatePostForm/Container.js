7101ce14b800952a3b584864ff3842bbd2841609
mward25 <61633012+mward25@users.noreply.github.com>
2020-03-01 09:57:12 -0800

    Update Container.js

--- client/src/components/CreatePostForm/Container.js
+++ client/src/components/CreatePostForm/Container.js
@@ -3,7 +3,6 @@ import { compose } from 'redux';
 import { reduxForm } from 'redux-form';
 import {
   titleValidator,
-  urlValidator,
   textPostValidator,
   typeValidator
 } from '../../util/validators';
@@ -15,12 +14,10 @@ import CreatePostForm from './Component';
 const validate = fields => {
   const errors = {};
   const title = fields.title ? fields.title : '';
-  const url = fields.url ? fields.url : '';
   const type = fields.type ? fields.type : '';
   const text = fields.text ? fields.text : '';
 
   errors.title = titleValidator(title);
-  if (type === 'link') errors.url = urlValidator(url);
   if (type === 'text') errors.text = textPostValidator(text);
   errors.type = typeValidator(type);
 
