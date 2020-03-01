9fc3a5a7976b051549b8e5752934437814278122
mward25 <61633012+mward25@users.noreply.github.com>
2020-02-29 17:53:37 -0800

    Update Empty.js

--- client/src/components/shared/Empty.js
+++ client/src/components/shared/Empty.js
@@ -22,7 +22,7 @@ const Wrapper = styled.div`
 `;
 
 const Empty = ({ comments }) => {
-  const message = comments ? 'no comments' : "there's nothing here..., be the first to post";
+  const message = comments ? 'no comments' : "there's nothing here...";
   return <Wrapper comments={comments}>{message}</Wrapper>;
 };
 
